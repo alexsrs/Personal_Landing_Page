bc90428c8ac551da8afe02e30360ed4f545108e6
Alex Sandro <alex@creapix.com.br>
2020-09-02 18:27:58 -0300

    slick

---
+++ js/slide.js
@@ -0,0 +1,69 @@
+$(function(){
+	$('.mosaico .container .mosaico-wraper').slick({
+		centerMode: false,
+		slidesToShow: 6,
+		arrows: false,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings:{
+					arrows: false,
+					centerMode: true,
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 580,
+				settings:{
+					arrows: false,
+					centerMode: true,
+					slidesToShow: 2
+				}
+
+			},
+			{
+				breakpoint: 380,
+				settings:
+					{
+						arrows: false,
+						centerMode: true,
+						slidesToShow: 1
+					}
+			}
+		]
+
+	});
+
+	$('.tratamentos .container').slick({
+		centerMode: false,
+		slidesToShow: 3,
+		arrows: false,
+		infinite: false,
+		responsive:[
+		{
+			breakpoint: 768,
+			settings:{
+				arrows: false,
+				dots: true,
+				infinite: false,
+				centerMode: false,
+				slidesToShow: 2
+			}
+		},
+
+		{
+			breakpoint: 545,
+			settings:{
+				arrows: false,
+				dots: true,
+				infinite: false,
+				centerMode: false,
+				slidesToShow: 1
+			}
+		}
+
+		]
+	});
+
+
+})
